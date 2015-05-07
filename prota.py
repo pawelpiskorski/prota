dcca639e95a50ff14ac6eec225f2f0593a1c417f
Paweł Piskorski <pawell.piskorski@gmail.com>
2015-05-07 12:05:36 +0200

    introduce Project class

--- prota.py
+++ prota.py
@@ -8,104 +8,145 @@ import argparse
 
 logging.basicConfig(level=logging.DEBUG)
 
-c_fname = "tasks.pickle"
-proj = {}
-pfile = None
 
-def load():
-	global pfile
-	global proj
+class Project:
+	def __init__(self, fname="tasks.pickle"):
 
-	if os.path.isfile(c_fname):
-		logging.debug("loading " + c_fname)
-		pfile = open(c_fname, 'r+')
-		proj = pickle.load(pfile)
-	else:
-		logging.error("no such file")
-		exit()
+		if os.path.isfile(fname):
+			logging.debug("loading " + fname)
+			f = open(fname, 'r+')
+			self.p = pickle.load(f)
+			f.close()
+		else:
+			logging.error("no such file")
+			self.p = {
+				'tid': 0,
+				't': {
+					0: {
+						'id': 0,
+						'm': 'the anonymous project',
+						'ch': []
+						# no pid defined in root task
+					}
+				}
+			}
 
+	def tasks(self, idlist):
+		for tid in idlist:
+			yield self.p['t'][tid]
 
-def save():
-	global pfile
-	global proj
-	
-	if pfile:
+	def add_task(self, task={}, pid=0, peer_id=0):
+		"""
+		inserts new task to list
+		:param task: template dict of the task
+		:param pid: parent id, root by default
+		:param peer_id: preceding task id. zero appends at the end, nonzero overrides pid with parent of the peer.
+		:return:
+		"""
+		parent = self.p['t'][pid]
+		peer = None
+		if peer_id:
+			peer = self.p['t'][peer_id]
+			pid = peer['pid']
+			parent = self.p['t'][pid]
+
+		tid = self.p['tid'] + 1
+		self.p['tid'] = tid
+		task['id'] = tid
+		task['pid'] = pid
+		task['ch'] = []
+		if 'm' not in task:
+			task['m'] = 'task ' + str(tid)
+		self.p['t'][tid] = task
+		if peer:
+			parent['ch'].insert(peer_id, tid)
+		else:
+			parent['ch'].append(tid)
+		return task
+
+	def childrenids(self, tid):
+		return self.p['t'][tid]['ch']
+
+	def children(self, tid):
+		return self.tasks(self.childrenids(tid))
+
+	def save(self, fname="tasks.pickle"):
 		logging.debug("saving file")
-		pickle.dump(proj, pfile)
-	else:
-		logging.error("file is None")
-		exit()
-	
-	
-def pprint(task, seek, indent):
-	if seek==0 or seek==task['id']:
-		print indent + '{0:05d}'.format(task['id']) + " " + task['m']
-		seek = 0
-	for subtask in task['t']:
-		pprint(subtask, indent+' ')
+		f = open(fname, 'w')
+		pickle.dump(self.p, f)
+		f.close()
+
+	def update(self, tid, task):
+		if 'id' in task:
+			del task['id']  # cannot change the task id
+		self.p['t'][tid].update(task)
+
+	def pprint(self, idlist, seek, indent=""):
+		for t in self.tasks(idlist):
+			if seek == 0 or seek == t['id']:
+				print indent + '{0:04d}'.format(t['id']) + " " + t['m']
+				self.pprint(t['ch'], 0, indent + " ")
+
+	def list(self, seek):
+		print self.p['t'][0]['m']
+		self.pprint(self.childrenids(0), seek, ' ')
+
 
 # commands
 
-def start(args):
-	global pfile
-	global proj
-	logging.info("starting project: " + args.m)
-	pfile = open(c_fname, 'w')
-	proj['tid'] = 0;
-	proj['t'] = {
-		'id' : 0,
-		'm' : args.m,
-		't' : []
+def start(params):
+	logging.info("starting projectect: " + params.m)
+	project = Project("")
+	task = {
+		'm': params.m}
+	project.update(0, task)
+	project.save()
+
+
+def add(params):
+	logging.info("adding task: " + params.m)
+	task = {
+		'm': params.m,
 	}
-	save()
 
-def add(args):
-	global pfile
-	global proj
-	logging.info("adding task: " + args.m)
-	load()
-	tid = proj['tid'] + 1
-	proj['tid'] = tid;
-	proj['t'] = {
-		'id' : tid,
-		'm' : args.m,
-		't' : []
-	}
-	save()
-	
-def ls(args):
-	global proj
-	logging.info("listing task: " + str(args.tid))
-	load()
-	pprint(proj['t'], args.tid, '')
+	project = Project()
 
-def edit(args):
-	global proj
-	logging.info("editing task: " + str(args.tid))
+	project.add_task(task, pid=params.parent, peer_id=params.after)
+	project.save()
 
-def move(args):
-	global proj
-	logging.info("moving task: " + str(args.tid))
 
-def remove(args):
-	global proj
-	logging.info("deleting task: " + str(args.tid))
+def ls(params):
+	logging.info("listing task: " + str(params.tid))
+	project = Project()
+	project.list(params.tid)
+
+
+def edit(params):
+	logging.info("editing task: " + str(params.tid))
+
+
+def move(params):
+	logging.info("moving task: " + str(params.tid))
+
+
+def remove(params):
+	logging.info("deleting task: " + str(params.tid))
 
 	
-parser = argparse.ArgumentParser(description='simple PROject TAsk management.')
+parser = argparse.ArgumentParser(description='simple projectect TAsk management.')
 subparsers = parser.add_subparsers()
 
-subparser = subparsers.add_parser('start', help="start new project")
-subparser.add_argument('m', type=unicode, help="project title")
+subparser = subparsers.add_parser('start', help="start new projectect")
+subparser.add_argument('m', type=unicode, help="projectect title")
 subparser.set_defaults(func=start)
 
-subparser = subparsers.add_parser('ls', help="list project file")
-subparser.add_argument('tid', type=int, default=0, nargs='?', help="id of task to print, 0 (whole project) by default")
+subparser = subparsers.add_parser('ls', help="list projectect file")
+subparser.add_argument('tid', type=int, default=0, nargs='?', help="id of task to print, 0 (whole projectect) by default")
 subparser.set_defaults(func=ls)
 
 subparser = subparsers.add_parser('add', help="add new task")
-subparser.add_argument('-p', '--new-parent', type=int, help="parent id for new task(s)")
-subparser.add_argument('-a', '--after', type=int, help="id of task that will precede new task")
+subparser.add_argument('-p', '--parent', type=int, default=0, nargs='?', help="parent id for new task")
+subparser.add_argument('-a', '--after', type=int, default=0, nargs='?', help="id of task that will precede new task")
 subparser.add_argument('-d', '--deadline', type=unicode, nargs='?', help="deadline")
 subparser.add_argument('-o', '--owner', type=unicode, nargs='?', default="", help="owner")
 subparser.add_argument('-s', '--state', type=unicode, nargs='?', default="new", help="task state")
@@ -114,7 +155,7 @@ subparser.set_defaults(func=add)
 
 subparser = subparsers.add_parser('e', help="edit task")
 subparser.add_argument('tid', type=int, default=0, help="task id")
-subparser.add_argument('-m', type=unicode, help="project title")
+subparser.add_argument('-m', type=unicode, help="projectect title")
 subparser.add_argument('-d', '--deadline', type=unicode, nargs='?', help="deadline")
 subparser.add_argument('-o', '--owner', type=unicode, nargs='?', help="owner")
 subparser.add_argument('-s', '--state', type=unicode, nargs='?', help="owner")
@@ -122,19 +163,19 @@ subparser.set_defaults(func=edit)
 
 subparser = subparsers.add_parser('rm', help="remove task")
 subparser.add_argument('tid', type=int, default=0, help="id of parent task")
-subparser.add_argument('m', type=unicode, help="project title")
+subparser.add_argument('m', type=unicode, help="projectect title")
 subparser.add_argument('-d', '--deadline', type=unicode, nargs='?', help="deadline")
 subparser.add_argument('-o', '--owner', type=unicode, nargs='?', help="owner")
 subparser.set_defaults(func=remove)
 
 subparser = subparsers.add_parser('mv', help="move task(s)")
 subparser.add_argument('trange', type=unicode, default=0, help="task range to move")
-subparser.add_argument('-p', '--new-parent', type=int, help="new parent for moved task(s)")
+subparser.add_argument('-p', '--parent', type=int, help="new parent for moved task(s)")
 subparser.add_argument('-a', '--after', type=int, help="id of task that will precede task range after move")
 subparser.set_defaults(func=move)
 
 
-args = parser.parse_args()
-args.func(args)
+params = parser.parse_args()
+params.func(params)
 
 
